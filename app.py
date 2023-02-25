329075defc751649471c20b826bc57c7544da093
thang truong <thangtruong@local-PC>
2023-02-26 02:29:30 +0700

    push code

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import requests
+import os
+
+API_KEY = os.getenv("API_KEY")
+#1db1db31def7dd7eb1e1d5b587c463de
+app = Flask(__name__)
+ 
+@app.route('/')
+def get_forecast():
+    city = "Ho Chi Minh City"
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(city,API_KEY)
+    print(url)
+    res = requests.get(url)
+    data = res.json()
+
+    return data
+
+@app.route('/readiness')
+def readiness():
+    return "Sucess"
+
+@app.route('/liveness')
+def liveness():
+    return "Sucess"
+ 
+if __name__ == '__main__':
+    app.run()
